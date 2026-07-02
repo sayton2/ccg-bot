5bbe83861a75ca100b9a96b14d8b13f8b5fb696f
sayton2 <45860531+sayton2@users.noreply.github.com>
2026-07-02 07:40:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -160,6 +160,9 @@ if __name__ == '__main__':
     bot_thread.daemon = True
     bot_thread.start()
     
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+    
 
 
 
